87de36eb244b42a125a3f188bc54d0396d3d849b
Rudra <rudraprajapati5102005@gmail.com>
2026-07-11 21:30:45 +0530

    feat(vehicle): wire search, delete, purchase, restock routes

--- backend/src/routes/vehicle.route.js
+++ backend/src/routes/vehicle.route.js
@@ -1,9 +1,22 @@
 import express from "express";
-import { addVehicle, getVehicles, putVehicle } from "../controller/vehicle.controller.js";
+
+import {
+  addVehicle,
+  getVehicles,
+  putVehicle,
+  searchVehicles,
+  deleteVehicle,
+  purchaseVehicle,
+  restockVehicle
+} from "../controller/vehicle.controller.js";
 
 const router = express.Router();
-
 router.post("/", addVehicle);
 router.get("/", getVehicles);
 router.put("/:id", putVehicle);
+router.get("/search", searchVehicles);
+router.delete("/:id", deleteVehicle);
+router.post("/:id/purchase", purchaseVehicle);
+router.post("/:id/restock", restockVehicle);
+
 export default router;
