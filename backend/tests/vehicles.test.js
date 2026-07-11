2992025d77581c2cbb57f4fe7c8ea1417d0e924f
Rudra <rudraprajapati5102005@gmail.com>
2026-07-11 18:56:49 +0530

    test(vehicle) 🔴 : Get all the vehicle

--- backend/tests/vehicles.test.js
+++ backend/tests/vehicles.test.js
@@ -27,4 +27,12 @@ describe("Vehicle API", () => {
     expect(res.statusCode).toBe(201);
     expect(res.body).toHaveProperty("id");
   });
+
+  it("should list all vehicles", async () => {
+  const res = await request(app).get("/api/vehicles");
+  expect(res.statusCode).toBe(200);
+  expect(Array.isArray(res.body)).toBe(true);
+});
+
+
 });
